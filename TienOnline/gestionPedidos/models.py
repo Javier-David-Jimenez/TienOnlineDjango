7054b63c863f9a64ede2fa26d97d63ce6f749858
Javier-David-Jimenez <142829587+Javier-David-Jimenez@users.noreply.github.com>
2023-09-21 17:44:55 +0200

    Inserted Data in PostgreSQL//Modified  class Article in models to give better answerss to filter
    Insertado datos en postgreSQL  en artículos
    Modificada la clase Articulos (en models) añadiéndole una función para  que nos devuelva los datos  de Articulos.Objects

--- TienOnline/gestionPedidos/models.py
+++ TienOnline/gestionPedidos/models.py
@@ -11,7 +11,14 @@ class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion= models.CharField(max_length=20)
     precio=models.IntegerField()
+    
+    def __str__(self): #Usamos funcion __str__ para cque nos devuelva un string
+        return 'El nombre es %s la sección es %s y el precio es %s' %(self.nombre, self.seccion, self.precio)
 
+"""cada vez que hacemos un cambio en el modelo debemos de volver a hacer las migracionessalimos de shell con -- exit() -- (si estamos en el) 
+        y  migramos  python manage.py makemigrations  y despues  python manage.py migrate   despues python manage.py shell para volver al shell
+       en shell Importamos la clase Articulos  from gestionPedidos.models import Articulos y despues usamos la clase Articulos.objects.filter(seccion'deportes')
+"""
 
 class Pedidos(models.Model):
     numero=models.IntegerField()
