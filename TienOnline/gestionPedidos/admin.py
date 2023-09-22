8fad242fb1e81e928021b79c4db6c07652f9d664
Javier-David-Jimenez <142829587+Javier-David-Jimenez@users.noreply.github.com>
2023-09-22 18:41:46 +0200

    Add 2 kind of filters adding new clases in admin.py using list_filter and date_hierarchy also adding list display
    Añadimos un filtro a  la seccion de Articulos. creando una clase en admin usando el list_filter
    
    Creamos otra clase para pedidos, para ver numero y fecha, poniendole un filtro también en fecha.
    
    y un filtro resumen en el top
    
    cambiamos el idioma de panel de admin en setings.py

--- TienOnline/gestionPedidos/admin.py
+++ TienOnline/gestionPedidos/admin.py
@@ -4,12 +4,21 @@ from django.contrib import admin
 from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 
-#creamos esta clase para que coja los datos que queremos de lod clientes en el panel de control
+#creamos esta clase para que coja los datos que queremos de los clientes en el panel de control
 class ClientesAdmin(admin.ModelAdmin):
     list_display=("nombre", "direccion", "tfno")
     search_fields=("nombre", "tfno")
 #con el search_fields añadimos un buscador en este caso de los campos nombre y tfno
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=("seccion",)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
 admin.site.register(Clientes, ClientesAdmin)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
 
