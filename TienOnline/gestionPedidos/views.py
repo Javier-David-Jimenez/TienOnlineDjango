ef7eaa15aebb1112d930bb31056c6e8151f659bd
Javier-David-Jimenez <142829587+Javier-David-Jimenez@users.noreply.github.com>
2023-09-29 10:42:51 +0200

    create a conexion betwen  a web and the data base to get an answer to a browse.
    crear un model con un  form con un cuadro para meter texto y un boton que busca ponerlo en views "busqueda_producto" y  en urls
    en views generar una funcion "buscar" con la respuesta con lo buscado, lo ponemos en url.py.
    
    Modificamos la views "buscar" que hemos creado  le damos un if con lapropiedad __icontains si el mensaje tiene datos (hace un select like en la BBDD)   Y en else ponemos un mensaje de que la petición de busqueda estaba en blanco
    
    generamos un html nuevo para ver los articulos encontrados con su nombre seccion y precio.

--- TienOnline/gestionPedidos/views.py
+++ TienOnline/gestionPedidos/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 # Create your views here.
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+    
+    #Con este if hacemos que si no introduce nada y le da a buscar, no envie la pregunta a la BBDD
+    if request.GET["prd"]:
+        #Esta variable toma prd y la une a Articulo buscado
+        #mensaje="Artículo buscado: %r" %request.GET["prd"]
+        
+        #producto coge el "prd" que han introducido en buscar
+        producto = request.GET["prd"]
+        
+        #la funcion nombre__icontain funciona como un filter de SQL  like nombre="producto"
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+        
+        return render(request, "resultados_busqueda.html", {"articulos":articulos, "query": producto})
+    else:
+        mensaje = "No has introducido nada"
+    return HttpResponse(mensaje)
